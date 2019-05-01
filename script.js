3b7f83986e0b5a7d109b52ca52de46cd80e3d3f9
Federico <fdsosa.35@gmail.com>
2019-05-01 16:54:39 -0300

    Doing responsive header and profile section(finished)

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ function displayLinks(){
     links.style.display = "flex";
     links.style.height = "30vh";
     document.getElementById("bg").style.margin = "43vh 0 0 0";
+    document.getElementById("bar").style.background = "#405284";
   }
   else
   {
     links.style.display = "none";
     links.style.height = "0vh";
     document.getElementById("bg").style.margin = "13vh 0 0 0";
+    document.getElementById("bar").style.background = "white";
   }
 
 }
