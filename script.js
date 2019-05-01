2f22f41df519636c0c08332dbe9eabd2133e09d9
Federico <fdsosa.35@gmail.com>
2019-05-01 15:32:25 -0300

    Doing responsive header and profile section(not finished)

---
+++ script.js
@@ -0,0 +1,18 @@
+function displayLinks(){
+
+  var links = document.getElementById("links-mob");
+
+  if (links.style.display != "flex")
+  {
+    links.style.display = "flex";
+    links.style.height = "30vh";
+    document.getElementById("bg").style.margin = "43vh 0 0 0";
+  }
+  else
+  {
+    links.style.display = "none";
+    links.style.height = "0vh";
+    document.getElementById("bg").style.margin = "13vh 0 0 0";
+  }
+
+}
