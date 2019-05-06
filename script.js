b5c67605b2cf07800a1d4199cc29233c870f740e
Federico <fdsosa.35@gmail.com>
2019-05-06 20:42:33 -0300

    Remaking js code

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-function displayLinks(){
+var menu_button = document.getElementById('bg-bars');
+    menu_button.addEventListener('click', toggleMenu);
 
-  var links = document.getElementById("links-mob");
+var mobile_menu = document.getElementById('links-mob');
+var sectionProfile = document.getElementById("bg");
+var bars = document.getElementById("bars");
 
-  if (links.style.display != "flex")
-  {
-    links.style.display = "flex";
-    links.style.height = "30vh";
-    document.getElementById("bg").style.margin = "43vh 0 0 0";
-    document.getElementById("bar").style.background = "#405284";
+function toggleMenu() {
+  if (mobile_menu.classList.contains('menu-hidden')) {
+    mobile_menu.classList.remove('menu-hidden');
+    mobile_menu.classList.add('menu-visible');
+    menu_button.style.backgroundColor = "#182153";
+    sectionProfile.style.margin = "43vh 0 0 0";
+    bars.style.color = "white";
+  } else {
+    mobile_menu.classList.remove('menu-visible');
+    mobile_menu.classList.add('menu-hidden');
+    menu_button.style.backgroundColor = "white";
+    sectionProfile.style.margin = "13vh 0 0 0";
+    bars.style.color = "#182153";
   }
-  else
-  {
-    links.style.display = "none";
-    links.style.height = "0vh";
-    document.getElementById("bg").style.margin = "13vh 0 0 0";
-    document.getElementById("bar").style.background = "white";
-  }
-
 }
