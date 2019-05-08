e22a37c1554d6a4c093e16319e83e325d5fb4595
Federico <fdsosa.35@gmail.com>
2019-05-07 22:00:57 -0300

    Adding animation in the mobile menu

--- script.js
+++ script.js
@@ -1,20 +1,77 @@
-function displayLinks(){
+var menu_button = document.getElementById('bg-bars');
+    menu_button.addEventListener('click', toggleMenu);
 
-  var links = document.getElementById("links-mob");
+var mobile_menu = document.getElementById('links-mob');
+var sectionProfile = document.getElementById("bg");
+var bars = document.getElementById("bars");
 
-  if (links.style.display != "flex")
+function toggleMenu() {
+  if (mobile_menu.classList.contains('menu-hidden'))
   {
-    links.style.display = "flex";
-    links.style.height = "30vh";
-    document.getElementById("bg").style.margin = "43vh 0 0 0";
-    document.getElementById("bar").style.background = "#405284";
+    animationMargin("bg", 0, 43, 3, true);
+    animationHeight("links-mob", 0, 30, 5, true);
+    mobile_menu.classList.remove('menu-hidden');
+    mobile_menu.classList.add('menu-visible');
+    menu_button.style.backgroundColor = "#182153";
+    bars.style.color = "white";
   }
   else
   {
-    links.style.display = "none";
-    links.style.height = "0vh";
-    document.getElementById("bg").style.margin = "13vh 0 0 0";
-    document.getElementById("bar").style.background = "white";
+    animationMargin("bg", 43, 13, 3, false);
+    animationHeight("links-mob", 30, 0, 5, false);
+    mobile_menu.classList.remove('menu-visible');
+    mobile_menu.classList.add('menu-hidden');
+    menu_button.style.backgroundColor = "white";
+    bars.style.color = "#182153";
   }
-
 }
+
+function animationMargin(id, posI, posF, ms, x){
+  var elem = document.getElementById(id);
+  var pos = posI;
+  var id = setInterval(frame, ms);
+  function frame() {
+    if (pos == posF) {
+      clearInterval(id);
+    } else {
+        if (x == true) {
+          pos++;
+        }else {
+          pos--;
+        }
+        elem.style.marginTop = pos + 'vh';
+    }
+  }
+}
+
+function animationHeight(id, posI, posF, ms, x){
+  var elem = document.getElementById(id);
+  var pos = posI;
+  var id = setInterval(frame, ms);
+  function frame() {
+    if (pos == posF) {
+      clearInterval(id);
+    } else {
+      if (x == true) {
+        pos++;
+      }else {
+        pos--;
+      }
+      elem.style.height = pos + 'vh';
+    }
+  }
+}
+
+// function animation2(){
+//   var elem = document.getElementById("links-mob");
+//   var pos = 0;
+//   var id = setInterval(frame, 10);
+//   function frame() {
+//     if (pos == 30) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.height = pos + 'vh';
+//     }
+//   }
+// }
