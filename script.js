7819bdbef8e44d7f1594d414e9018c866b1cdc41
Federico <fdsosa.35@gmail.com>
2019-05-10 18:50:21 -0300

    feature/details - Create a new css file to divide the responsive code

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var menu_button = document.getElementById('bg-bars');
     menu_button.addEventListener('click', toggleMenu);
 
+function func(){
+    var showFunction = setTimeout(showMenu, 8000);
+}
+
 var mobile_menu = document.getElementById('links-mob');
 var sectionProfile = document.getElementById("bg");
 var bars = document.getElementById("bars");
@@ -8,8 +12,9 @@ var bars = document.getElementById("bars");
 function toggleMenu() {
   if (mobile_menu.classList.contains('menu-hidden'))
   {
-    animationMargin("bg", 0, 43, 3, true);
-    animationHeight("links-mob", 0, 30, 5, true);
+    func()
+    // animationMargin("bg", 0, 43, 3, true);
+    // animationHeight("links-mob", 0, 30, 5, true);
     mobile_menu.classList.remove('menu-hidden');
     mobile_menu.classList.add('menu-visible');
     menu_button.style.backgroundColor = "#182153";
@@ -17,8 +22,8 @@ function toggleMenu() {
   }
   else
   {
-    animationMargin("bg", 43, 13, 3, false);
-    animationHeight("links-mob", 30, 0, 5, false);
+    // animationMargin("bg", 43, 13, 3, false);
+    // animationHeight("links-mob", 30, 0, 5, false);
     mobile_menu.classList.remove('menu-visible');
     mobile_menu.classList.add('menu-hidden');
     menu_button.style.backgroundColor = "white";
@@ -26,51 +31,41 @@ function toggleMenu() {
   }
 }
 
-function animationMargin(id, posI, posF, ms, x){
-  var elem = document.getElementById(id);
-  var pos = posI;
-  var id = setInterval(frame, ms);
-  function frame() {
-    if (pos == posF) {
-      clearInterval(id);
-    } else {
-        if (x == true) {
-          pos++;
-        }else {
-          pos--;
-        }
-        elem.style.marginTop = pos + 'vh';
-    }
-  }
+function showMenu(){
+  mobile_menu.classList.add('transitionAux')
 }
 
-function animationHeight(id, posI, posF, ms, x){
-  var elem = document.getElementById(id);
-  var pos = posI;
-  var id = setInterval(frame, ms);
-  function frame() {
-    if (pos == posF) {
-      clearInterval(id);
-    } else {
-      if (x == true) {
-        pos++;
-      }else {
-        pos--;
-      }
-      elem.style.height = pos + 'vh';
-    }
-  }
-}
-
-// function animation2(){
-//   var elem = document.getElementById("links-mob");
-//   var pos = 0;
-//   var id = setInterval(frame, 10);
+// function animationMargin(id, posI, posF, ms, x){
+//   var elem = document.getElementById(id);
+//   var pos = posI;
+//   var id = setInterval(frame, ms);
 //   function frame() {
-//     if (pos == 30) {
+//     if (pos == posF) {
 //       clearInterval(id);
 //     } else {
-//       pos++;
+//         if (x == true) {
+//           pos++;
+//         }else {
+//           pos--;
+//         }
+//         elem.style.marginTop = pos + 'vh';
+//     }
+//   }
+// }
+//
+// function animationHeight(id, posI, posF, ms, x){
+//   var elem = document.getElementById(id);
+//   var pos = posI;
+//   var id = setInterval(frame, ms);
+//   function frame() {
+//     if (pos == posF) {
+//       clearInterval(id);
+//     } else {
+//       if (x == true) {
+//         pos++;
+//       }else {
+//         pos--;
+//       }
 //       elem.style.height = pos + 'vh';
 //     }
 //   }
