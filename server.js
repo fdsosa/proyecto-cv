5667c90bf7048b3c3c236583e6e7940195cde269
Federico <fdsosa.35@gmail.com>
2019-06-23 20:44:32 -0300

    Solving problems with the deploy

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 var express = require('express');
 var server = express();
-var path = require('path')
+var path = require('path');
+var port = process.env.PORT || 8080;
+var env = process.env.ENV || 'DEV';
+var folderBuild = env == 'PROD' ? '/dist/' : '/.temp/';
+
+server.use(express.static(path.join(__dirname, folderBuild)));
 
 server.get('/', function (req, res) {
-  //console.log('Esta es la raiz de mi sitio');
-  //req.status(200).send('Bienvenido a mi servidor');
-  return res.sendFile(path.join(__dirname, '/dist/index.html'));
+  // console.log('Esta es la raiz de mi sitio');
+  // res.status(200).send('Bienvenido a mi servidor');
+  return res.sendFile(path.join(__dirname, folderBuild, 'index.html'));
+});
+
+server.get('/home', function (req, res) {
+  // console.log('Esta es la raiz de mi sitio');
+  // res.status(200).send('Bienvenido a mi servidor');
+  return res.sendFile(path.join(__dirname, folderBuild, 'index.html'));
 });
 
 server.get('/contact', function (req, res) {
-  res.status(200).send('Pagina de Contacto');
+  // res.status(200).send('Pagina de Contacto');
+  return res.sendFile(path.join(__dirname, folderBuild, 'contact.html'));
 });
 
-server.listen(process.env.PORT || 8080, function () {
-  console.log('Mi servidor esta en linea en el puerto 2500');
+server.listen(port, function () {
+  console.log('Mi servidor esta en linea en el puerto ' + port);
 });
