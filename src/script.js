91a7d20ba30df1be77ad10d896300acf989dfb94
Federico <fdsosa.35@gmail.com>
2019-07-25 19:59:01 -0300

    feature/animations - Solved problems

--- src/script.js
+++ src/script.js
@@ -2,8 +2,6 @@ var menu_button = document.getElementById('bg-bars');
     menu_button.addEventListener('click', toggleMenu);
 
 var mobile_menu = document.getElementById('links-mob');
-    mobile_menu.addEventListener('transitionstart', showMenu);
-    mobile_menu.addEventListener('transitionend', hideMenu);
 
 var sectionProfile = document.getElementById("bg");
 var bars = document.getElementById("bars");
@@ -28,18 +26,6 @@ function toggleMenu() {
     menu_button.classList.remove('bar-active');
     menu_button.classList.add('bar-inactive');
     bars.style.color = "#182153";
-  }
-}
-
-function showMenu(){
-  if (mobile_menu.classList.contains('menu-visible')) {
-    mobile_menu.classList.remove('d-none')
-  }
-}
-
-function hideMenu(){
-  if (mobile_menu.classList.contains('menu-hidden')) {
-    mobile_menu.classList.add('d-none')
   }
 }
 
